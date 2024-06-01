e50f39a242e02a67641d770dad4821d3987c13b4
jeanneloise <togononjeanne11@gmail.com>
2024-06-01 22:48:30 +0400

    mywebsite//Jeanne Togonon

---
+++ cart.js
@@ -0,0 +1,376 @@
+const product = [
+    {
+        id: 0,
+        image: '/bracelet/1.png',
+        title: 'Silver Gnoce Bracelet',
+        price: 80,
+    },
+    {
+        id: 1,
+        image: '/bracelet/3.png',
+        title: 'Black Gnoce Bracelet',
+        price: 80,
+    },
+    {
+        id: 2,
+        image: '/bracelet/2.png',
+        title: 'Silver Italian Bracelet ',
+        price: 60,
+    },
+    {
+        id: 3,
+        image: '/bracelet/5.png',
+        title: 'Gold Heart Italian Bracelet',
+        price: 75,
+    },
+    {
+        id: 4,
+        image: '/bracelet/6.png',
+        title: 'Silver butterfly Italian Bracelet',
+        price: 65,
+    },
+    {
+        id: 5,
+        image: '/bracelet/7.png',
+        title: 'Black Italian Bracelet',
+        price: 60,
+    },
+    {
+        id: 6,
+        image: '/bracelet/8.png',
+        title: 'Rose Gold Italian Bracelet',
+        price: 80,
+    },
+    {
+        id: 7,
+        image: '/bracelet/9.png',
+        title: 'Gold Star italian Bracelet',
+        price: 75,
+    },
+    {
+        id: 8,
+        image: '/bracelet/10.png',
+        title: 'Gold Italian Bracelet',
+        price: 70,
+    },
+    {
+        id: 9,
+        image: '/bracelet/11.png',
+        title: 'Silver Star Italian Bracelet',
+        price: 65,
+    },
+    {
+        id: 10,
+        image: '/bracelet/12.png',
+        title: 'Rainbbow Italian Bracelet',
+        price: 60,
+    },
+    {
+        id: 11,
+        image: '/bracelet/13.png',
+        title: 'Rose Gold Heart Italian Bracelet',
+        price: 80,
+    },
+    {
+        id: 12,
+        image: '/bracelet/14.png',
+        title: 'Rose Gold Start Italian bracelet',
+        price: 80,
+    },
+    {
+        id: 13,
+        image: '/charms/a.jpg',
+        title: 'Basketball Charm',
+        price: 25,
+    },
+    {
+        id: 14,
+        image: '/charms/b.jpg',
+        title: '"Be Mine" Charm',
+        price: 25,
+    },
+    {
+        id: 15,
+        image: '/charms/c.jpg',
+        title: 'Stars Charm',
+        price: 25,
+    },
+    {
+        id: 16,
+        image: '/charms/d.jpg',
+        title: 'Cat charm',
+        price: 25,
+    },
+    {
+        id: 17,
+        image: '/charms/e.jpg',
+        title: 'Rainbow Charm',
+        price: 25,
+    },
+    {
+        id: 18,
+        image: '/charms/f.jpg',
+        title: 'Evil eye Charm',
+        price: 25,
+    },
+    {
+        id: 19,
+        image: '/charms/g.jpg',
+        title: 'Cherry Charm',
+        price: 25,
+    },
+    {
+        id: 20,
+        image: '/charms/h.jpg',
+        title: 'Racing Flag Charm',
+        price: 25,
+    },
+    {
+        id: 21,
+        image: '/charms/i.jpg',
+        title: 'Halloween pumpkin cat Charm',
+        price: 25,
+    },
+    {
+        id: 22,
+        image: '/charms/j.jpg',
+        title: 'Sleeping car Charm',
+        price: 25,
+    },
+    {
+        id: 23,
+        image: '/charms/k.jpg',
+        title: 'Pker car Symbol Charm',
+        price: 25,
+    },
+    {
+        id: 24,
+        image: '/charms/l.jpg',
+        title: 'Campfire Charm',
+        price: 25,
+    },
+    {
+        id: 25,
+        image: '/charms/m.jpg',
+        title: 'Cactus Charm',
+        price: 25,
+    },
+    {
+        id: 26,
+        image: '/charms/n.jpg',
+        title: 'Cabin Charm',
+        price: 25,
+    },
+    {
+        id: 27,
+        image: '/charms/o.jpg',
+        title: 'Butterfly Charm',
+        price: 25,
+    },
+    {
+        id: 28,
+        image: '/charms/p.jpg',
+        title: 'Hamburger Charm',
+        price: 25,
+    },
+    {
+        id: 29,
+        image: '/charms/q.jpg',
+        title: 'Rabbit Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/charms/r.jpg',
+        title: 'Bee Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/charms/s.jpg',
+        title: 'Milktea Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/charms/u.jpg',
+        title: 'Coconut Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/charms/v.jpg',
+        title: 'Clover Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/aa.jpg',
+        title: 'Halloween Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/bb.jpg',
+        title: '"Good Luck" Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/cc.jpg',
+        title: 'Skull Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/dd.jpg',
+        title: 'Halloween Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/ee.jpg',
+        title: 'Elegant Roll Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/ff.jpg',
+        title: 'Diamond Heart Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/ggg.jpg',
+        title: 'Gentleman Skull Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/hh.jpg',
+        title: 'Crown Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/ii.jpg',
+        title: 'Snake Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/jj.jpg',
+        title: 'Heart Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/kk.jpg',
+        title: 'Diamond roll Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/ll.jpg',
+        title: 'Bat Character Gnoce Charm',
+        price: 25,
+    },
+    {
+        id: 30,
+        image: '/Gnoce1/mm.jpg',
+        title: 'Hope Gnoce Charm',
+        price: 25,
+    },
+
+    {
+        id: 30,
+        image: '/Gnoce1/nn.jpg',
+        title: 'Cat on a moon Gnoce Charm',
+        price: 25,
+    },
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>$ ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$ "+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "$ "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+
+    
+}
